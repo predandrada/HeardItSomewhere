262a49a596667ec5977bee2fcde3ac3cd9913944
predandrada <predandrada@yahoo.ro>
2020-03-22 00:00:36 +0200

    Third commit: created Spotify client & methods to work with.

--- app_engine.py
+++ app_engine.py
@@ -1,7 +1,14 @@
 # Using the Youtube API
+# coding=utf-8
+
+import re
+import json
+import requests
+import spotipy
 
 from auth import client_id
 from auth import client_secret
+from auth import spotify_token
 
 from googleapiclient.discovery import build
 import google_auth_oauthlib
@@ -54,7 +61,6 @@ class YoutubeEngine:
     def look_up_playlist(self, playlist_name):
         for item in self.playlists["items"]:
             if item["snippet"]["title"].lower() == playlist_name.lower():
-                # print("FOUND THE PLAYLIST! The id is: ")
                 return item["id"]
         return -1
 
@@ -64,7 +70,6 @@ class YoutubeEngine:
         if playlist_id == -1:
             return None
 
-        print(playlist_id)
         request = self.yt_client.playlistItems().list(
             part="snippet",
             maxResults=35,
@@ -81,9 +86,12 @@ class YoutubeEngine:
             # some songs might not have the song name & artist set up
             # so they will be skipped because their query would generate incorrect responses
             if video["artist"] is not None and video["track"] is not None:
+                artist = re.sub(r'\W+', ' ', video["artist"])
+                track = re.sub(r'\W+', ' ', video["track"])
+                print(artist + "          " + track)
                 song_collection[id] = {
-                    "artist": video["artist"],
-                    "track": video["track"]
+                    "artist": artist,
+                    "track": track
                 }
 
 
@@ -91,11 +99,11 @@ class YoutubeEngine:
 class AppEngine:
     def __init__(self):
         self.youtube_engine = YoutubeEngine()
-        # self.spotify_engine = SpotifyEngine()
+        self.spotify_engine = SpotifyEngine()
         self.song_collection = {}
 
     # the song collection gets updated
-    def export(self, playlist_name):
+    def gather_data(self, playlist_name):
         self.youtube_engine.get_songs(playlist_name, self.song_collection)
         print(self.song_collection)
 
@@ -106,17 +114,118 @@ class AppEngine:
         return playlist_to_look_up
 
     def run(self):
-        self.export(self.input_playlist())
+        self.gather_data(self.input_playlist())
+        self.export()
+
+    def export(self):
+        self.spotify_engine.add_songs_to_playlist(self.song_collection)
 
 
 class SpotifyEngine:
     def __init__(self):
-        print("this is the spotify engine")
+        self.extra = Extra()
 
-    def get_client(self):
+    # creates a new playlist and returns its id
+    def create_spotify_playlist(self, user_id, playlist_name):
+        request = json.dumps({
+            "name": playlist_name,
+            "description": "Playlist imported from Youtube through HeardItSomewhere",
+            "public": True
+        })
+
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(
+            user_id
+        )
+        response = requests.post(
+            query,
+            data=request,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(spotify_token)
+            }
+        )
+        return response.json()["id"]
+
+    # returns the song uris of a given song & artist
+    def get_song_uri(self, track, artist):
+        query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track".format(
+            track,
+            artist
+        )
+        response = requests.get(
+            query,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(spotify_token)
+            }
+        )
+        response = response.json()
+        # case in which no tracks are found
+        if response["tracks"]["total"] == 0:
+            return None
+        return response["tracks"]["items"][0]["id"]
+
+    def fetch_all_uri(self, song_collection):
+        uris = []
+        for key in song_collection.keys():
+            uri = self.get_song_uri(song_collection[key]["track"], song_collection[key]["artist"])
+            if uri is not None:
+                uris.append(uri)
+        return uris
+
+    def add_songs_to_playlist(self, song_collection):
+        user_id = input("Please type in your spotify user id: ")
+        playlist_name = input("What would you like to call your Spotify playlist? ")
+        playlist_id = self.create_spotify_playlist(user_id, playlist_name)
+        uris = self.fetch_all_uri(song_collection)
+
+        request = json.dumps(uris)
+        query = "https://api.spotify.com/v1/playlists/{}/tracks".format(
+            playlist_id
+        )
+        response = requests.post(
+            query,
+            data=request,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(spotify_token)
+            }
+        )
+        if response.status_code != 200:
+            print("asta este!")
+
+        response = response.json()
+        print(response)
+        return response
+
+
+class Extra:
+    def __init__(self):
         pass
+
+    def strip_korean(self, string):
+        en_list = re.findall(u'[^\uAC00-\uD7A3]', string)
+        for c in string:
+            if c not in en_list:
+                string = string.replace(c, '')
+        return string
+
+    def strip_japanese(self, string):
+        en_list = re.findall(u'[^\3040-\u309F]', string)
+        for c in string:
+            if c not in en_list:
+                string = string.replace(c, '')
+        return string
+
+    def strip_chinese(self, string):
+        en_list = re.findall(u'[^\u4E00-\u9FA5]', string)
+        for c in string:
+            if c not in en_list:
+                string = string.replace(c, '')
+        return string
 
 
 if __name__ == "__main__":
     AppEngine().run()
     print("Thank you for using HeardItSomewhere! See you next time!")
+
